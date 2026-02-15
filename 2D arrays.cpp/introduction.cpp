19459f4dc3a8bda92e5972edb7aa6de0249dece5
devraj-sonu <devraj6477@gmail.com>
2026-02-15 11:11:40 +0530

    Add basic changes in the folder

--- 2D arrays.cpp/introduction.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-    int students[3][3] = {{100, 100, 100},
-                            {64, 78, 67},
-                            {84,94,56}};
-}
